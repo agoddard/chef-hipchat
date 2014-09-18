baedcb107e288654f31bf854d7e024d83e8e5db1
Anthony Goddard <anthony@anthonygoddard.com>
2014-09-17 22:04:27 -0600

    yolo rm -rf

--- attributes/handler.rb
+++
@@ -1,8 +0,0 @@
-# values are required for token and room attrs
-default['hipchat']['handler']['token'] = nil
-default['hipchat']['handler']['room'] = nil
-default['hipchat']['handler']['enabled'] = false
-# values for name, color and notify_users attrs are optional
-default['hipchat']['handler']['name'] = nil
-default['hipchat']['handler']['color'] = nil
-default['hipchat']['handler']['notify_users'] = nil
